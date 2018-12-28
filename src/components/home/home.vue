5f9230d39c8871122ffc6a6efd70149e722db40c
kuiningchen <13288651469@163.com>
2018-12-28 14:04:23 +0800

    'init'

---
+++ src/components/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">师问</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import './home.scss';
+</style>
